b4aa3ba8ee160d26545cb35cbb3ec15d3d9114da
chick_0 <chick_0@ch1ck.xyz>
2022-07-19 09:18:42 +0900

    first commit

---
+++ vite.config.js
@@ -0,0 +1,27 @@
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import purgecss from "@fullhuman/postcss-purgecss";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [svelte()],
+
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: '@use "src/variables.scss" as *;',
+            },
+        },
+
+        postcss: {
+            plugins: [
+                purgecss({
+                    content: ["./src/**/*.svelte"],
+                    safelist: ["html", "body"],
+                }),
+            ],
+        },
+    },
+
+    define: {},
+});
