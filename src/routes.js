75929ed371be062d0c975059f500aec955084ec9
chick_0 <chick_0@ch1ck.xyz>
2022-07-25 21:52:08 +0900

    remove my projects page

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,6 @@
 import Login from "./routes/Login.svelte";
 import Dashboard from "./routes/Dashboard.svelte";
 
-import ProjectList from "./routes/ProjectList.svelte";
 import ProjectCreate from "./routes/ProjectCreate.svelte";
 import Project from "./routes/Project.svelte";
 
@@ -10,8 +9,6 @@ import NotFound from "./NotFound.svelte";
 export default {
     "/": Login,
     "/dashboard": Dashboard,
-
-    "/project": ProjectList,
     "/project/create": ProjectCreate,
     "/project/:uuid": Project,
 
--- src/routes/Dashboard.svelte
+++ src/routes/Dashboard.svelte
@@ -1,9 +1,12 @@
 <script>
     import { push } from "svelte-spa-router";
-    import { clearToken, getPayload } from "../token.js";
+    import { PROJECT_LIST } from "../url.js";
+    import { clearToken, getToken, getPayload } from "../token.js";
+    import { getLabel, getColor } from "../type.js";
 
     let payload = getPayload();
     let end_of_session = "...";
+    let projectList = [];
 
     if (payload == undefined) {
         clearToken();
@@ -17,6 +20,24 @@
         } else {
             end_of_session = new Date(payload.exp * 1000).toLocaleTimeString();
         }
+
+        fetch(PROJECT_LIST, {
+            method: "GET",
+            headers: {
+                Authorization: getToken(),
+            },
+        })
+            .then((resp) => resp.json())
+            .then((json) => {
+                if (json.detail != undefined) {
+                    alert(json.detail.msg);
+                } else {
+                    projectList = json.projectList;
+                }
+            })
+            .catch(() => {
+                alert("네트워크 오류가 발생했습니다.");
+            });
     }
 </script>
 
@@ -26,23 +47,7 @@
         <p class="subtitle">세션 만료 시간 : <b>{end_of_session}</b></p>
 
         <div class="box">
-            <h5 class="title is-5">프로젝트</h5>
-            <div class="buttons">
-                <a class="button is-primary" href="#/project">내 프로젝트</a>
-                <a class="button is-warning" href="#/project/create">프로젝트 생성</a>
-            </div>
-        </div>
-
-        <div class="box">
-            <h5 class="title is-5">배포 토큰</h5>
-            <div class="buttons">
-                <a class="button is-success" href="#/deploy-token/create">배포 토큰 생성</a>
-                <a class="button is-info" href="#/deploy-token">배포 토큰 조회</a>
-            </div>
-        </div>
-
-        <div class="box">
-            <h5 class="title is-5">계정</h5>
+            <h5 class="title is-5">계정 관리</h5>
             <div class="buttons">
                 <button
                     class="button is-dark"
@@ -54,3 +59,29 @@
         </div>
     </div>
 </section>
+
+<section class="section">
+    <div class="container">
+        <h2 class="title is-2">내 프로젝트</h2>        
+        <div class="block">
+            <a class="button is-link" href="#/project/create">프로젝트 생성</a>
+        </div>
+
+        <div class="block">
+            {#each projectList as project}
+                <div
+                    class="box is-clickable"
+                    on:click="{() => {
+                        push(`/project/${project.uuid}`);
+                    }}">
+                    <b class="tag {getColor(project.type)}" style="width: 86px;">{getLabel(project.type)}</b>
+                    {project.title}
+
+                    <div class="block"></div>
+                </div>
+            {/each}
+        </div>
+    </div>
+</section>
+
+<section class="section"></section>
