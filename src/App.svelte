b86d7de92c4780760c85ecdd8d4fc5866ddd9cd7
chick_0 <chick_0@ch1ck.xyz>
2022-07-25 22:03:41 +0900

    use location store for hide in login and show in other

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,25 @@
 <script>
     import Router from "svelte-spa-router";
+    import { location } from "svelte-spa-router";
     import routes from "./routes.js";
 
     if (window.location.pathname.indexOf("index.html") == 1) {
         window.location.replace("/" + window.location.hash);
     }
+
+    let hide = true;
+
+    location.subscribe((path) => {
+        if (path == "/") {
+            hide = true;
+        } else {
+            hide = false;
+        }
+    });
 </script>
 
 <Router routes="{routes}" />
 
-{#if window.location.hash != "" && window.location.hash != "#/"}
+{#if hide == false}
     <section class="section"></section>
 {/if}
